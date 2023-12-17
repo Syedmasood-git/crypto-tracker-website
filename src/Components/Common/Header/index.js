bbc044c5e6f224513c27231b04eff8024909d182
Syedmasood-git <masoodhussaini46@gmail.com>
2023-12-17 09:07:15 +0530

    done

--- src/Components/Common/Header/index.js
+++ src/Components/Common/Header/index.js
@@ -7,7 +7,7 @@ import { Link } from 'react-router-dom'
 const Header = () => {
   return (
     <div className='navbar'>
-      <h1 className='logo'>CryptoTracker<span style={{color:"var(--blue)"}}>.</span></h1>
+      <Link to='/'><h1 className='logo'>CryptoTracker<span style={{color:"var(--blue)"}}>.</span></h1></Link>
       <div className='links'>
       <Link to='/'><p className='link'>Home</p></Link>
                 <Link to='/compare'><p className='link'>Compare</p></Link>
